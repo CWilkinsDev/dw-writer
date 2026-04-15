4c3bedbae16d03b6c483d4e60b08ce386d5c7d5b
Charlie Wilkins <charlie@charliewilkins.tech>
2026-04-15 17:49:39 +0200

    Update text on Index page

--- public_html/index.php
+++ public_html/index.php
@@ -28,71 +28,60 @@
                         alt="A portrait photograph of the author from the waist up" />
                 </div>
                 <p>
-                    So, this is my website. Welcome.
-                </p>
-                <p>
-                    I'm David Wilkins and since I retired from work at the age of sixty in 2015, I've written - or
-                    rather, I've partly-written and partly-assembled - a couple of books. The first one was quite a
-                    small project, a personal challenge, to see if I could do it. It was moderately successful but
-                    largely only of regional interest in the West Country. The second -
-                    <a href="./longest-walk.php"><i>The Longest Walk Home</i></a>
-                    - has sold
-                    very well in its market and received lots of positive reviews. It is that book that is my motivation
-                    for putting this website together. I'm a private person. I have no social media accounts - and I
-                    would truly rather not write about myself online. But I've been encouraged to make this website by
-                    people who know more about these things than I. If you want to write books, these people said, you
-                    need to put yourself out there. This was clearly good advice whether I liked it or not. So I'm
-                    following it.
+                    Welcome. I'm David Wilkins and since I retired from work at the age of sixty in 2015, I've written -
+                    or rather, I've partly written and partly edited - a couple of books from original manuscripts by
+                    other people. The first book was quite a small project, a personal challenge, to see if I could do
+                    it. It was moderately successful but largely only of regional interest in the West Country. The
+                    second - <i>The Longest Walk Home</i> - has sold very well in its market and received lots of
+                    positive
+                    reviews. It is also the book that is my motivation for putting this website together. I'm a private
+                    person. I have no social media accounts. I would truly rather not write about myself online. But
+                    I've been encouraged to make this website by people who know more about these things than I do. If
+                    you want to write books, you need to 'put yourself out there', is the advice. I can see that this
+                    makes sense whether I like it or not. So here I am.
                 </p>
                 <p>
                     It's a strange experience to publish a couple of books when you are in your sixties (as I was for
                     the first one) or your seventies, as I was for the second. In the latter case, it's also been really
-                    quite disorientating because the book has done well. It's been like taking the wrong turn on a
-                    familiar footpath and finding oneself somehow in a strange and magical forest. Suddenly there's a
-                    whole new world to understand and explore. It's been good fun.
+                    quite disorientating, mainly because the book has done very well, despite its rather niche market
+                    crossover of military history / human interest. Creating a successful book has been like taking the
+                    wrong turn on a familiar footpath and finding oneself in an enchanted forest - if that's not a too
+                    fanciful a way of describing the world of publishing. Suddenly there are lots of new people and
+                    processes in your life. There are editors and proof-readers; marketing teams and PR people and legal
+                    advisors. There are serious contracts to sign. There are audition tapes to listen to so you can
+                    choose the right voice for audio book. If there's some little detail you aren't happy with in the
+                    cover design, the illustrator will probably change it for you. And there are bookshops in every city
+                    that have your book on the shelves. You can go into any of these bookshops anytime you like, gaze
+                    fondly at your own book and feel pleased with yourself. No-one can stop you. In fact, if you
+                    overcome your natural reticence about putting yourself forward, you can even take your book to the
+                    counter, introduce yourself and offer to sign all the copies the shop has in stock. And if you do
+                    that, the sales assistant will, without fail, be delighted because signed books sell better.
+                    Surprisingly also, in my experience, the sales assistant will never ask you to prove you are who you
+                    say you are; they will just take your word for it. It's all been a strange experience but it's been
+                    an adventure and it's been fun. I'm very grateful to have had it in my life.
                 </p>
                 <p>
-                    I started reading books when I was at infants' school in the early 1960s and I've not stopped since.
-                    I've read loads. But until I had a crack at it myself I'd never really thought about how difficult
-                    it is to create a book. In the first place, you have to research, write and edit the thing. In the
-                    case of <i>The Longest Walk Home</i>, this took hundreds of hours and for me, was every bit as slow
-                    and
-                    tortuous a process as all those celebrated authors say it is. Next, you have to find a publisher.
-                    That was certainly the most difficult bit for me, as it probably is I think for most aspiring
-                    writers. Ten publishers either turned me down flat or didn't even bother to reply. I began to
-                    consider giving up - but instead, on the advice of an old friend - I decided on an alternative
-                    approach. I took matters into my own hands. It's a long story but that decision paid off. A
-                    publisher - Quercus - found me. I'll be forever grateful both to them and to my friend Jim, who gave
-                    me the advice.
-                </p>
-                <p>
-                    Once you have a publisher you are properly in the mysterious new world. There are people there whose
-                    sole job it is to make helpful suggestions about various small ways in which you might improve your
-                    book. This was initially quite a surprise to me but turned out to be a kind and considered process
-                    that made the book better. And while that is happening others are getting busy too. I pictured a
-                    kind of elfin workshop. There was contractual and legal stuff to be done, probably by elderly gents
-                    scratching away at the pages of enormous leather-bound ledgers with quill pens. An artist in a smock
-                    stood at an easel and painted a picture for the cover. The book was typeset by bespectacled chaps
-                    picking up tiny individual letters with tweezers, and printed on a huge clanking press with steam
-                    leaking out of the pipes. And in the real world meanwhile, smart, efficient, young people were
-                    promoting the book, arranging for it to be distributed and encouraging booksellers to stock it.
-                    Getting to the stage of publication once you have a publisher seems relatively quick - but the whole
-                    thing from start to finish takes an age - or at least it did in my case. <i>The Longest Walk
-                        Home</i> took
-                    more than six years from my starting work on it at my kitchen table to the joyous little miracle of
-                    seeing it on bookshop shelves.
-                </p>
-                <p>
-                    So, there's a lot more to having a book published than you might think. If you want to know more
-                    about my discovery of the two found manuscripts that form the core of my two books, then you can
-                    find the stories behind both of them by clicking here:
-                    <a href="blog/behind-the-stories.php">The Story Behind The Stories.</a>
-                    If you are
-                    interested in the process of achieving publication, then you can read about my personal experience
-                    of that here:
-                    <a href="blog/road-to-publication.php">The Long Road to Publication.</a>
-                    In the unlikely event that you want to know more about me,
-                    <a href="./biography.php">click here to read a short biography.</a>
+                    I love books. I learned to read when I was at infants' school in the early 1960s and I've not
+                    stopped reading since. A rough calculation suggests I've managed comfortably over 1500 books so far
+                    in my life. But until I had a crack at it myself I'd never really thought about how difficult it is
+                    actually to make a book actually happen. In the first place, you have to research, write and edit
+                    the thing. In the case of <i>The Longest Walk Home</i>, this took hundreds of hours and for me, was
+                    every
+                    bit as slow and tortuous a process as those celebrated authors always say it is. Next, you have to
+                    find a publisher. That was certainly the most difficult bit for me, as it probably is, I think, for
+                    most aspiring writers. Ten publishers either turned me down flat or didn't reply. For a short while
+                    I even considered giving up on the whole idea - but instead, on the advice of an old friend, I
+                    decided on an alternative approach that put me more in control of events. It's a long story - but
+                    that decision paid off. It resulted in a publisher - Quercus - finding me. I'll be forever grateful
+                    both to them and to my friend Jim, who gave me the intitial advice and made a massive contribution
+                    to the process of getting the book published. It was more than six years between my starting work on
+                    <i>The Longest Walk Home</i> at my kitchen table to the joyous little miracle of seeing it on
+                    bookshop
+                    shelves. You can find the full story of the book's long road to publication on the <a
+                        href="./books.php">Books</a> page of
+                    this website. Or in the unlikely event that you want to know more about me, you can click on the
+                    link to my
+                    <a href="./biography.php">Biography</a>.
                 </p>
                 <p style="text-indent: 100px;">
                     David W.
