f63a70eb38aa14f5c879fa8c208baa5178331f13
Charlie Wilkins <charlie@charliewilkins.tech>
2026-04-15 17:49:57 +0200

    Implement Biography page with image

--- public_html/biography.php
+++ public_html/biography.php
@@ -21,10 +21,45 @@
             <div class="pagetitle">
                 <h2>Biography</h2>
             </div>
-            <div class="contentblock">
+            <div class="contentblock" id="floatingimagecontainer">
+                <div id="floatingimage">
+                    <img src="images/david-wilkins2_499x400.jpg"
+                        alt="A second portrait photograph of the author from the waist up" />
+                </div>
                 <p>
-                    A native of Derbyshire,
-                    David Wilkins lives on the Isle of Portland in Dorset.
+                    I spent most of my working life in the charitable and public sectors. My work largely involved
+                    campaigning, research and policy development in relation to social issues such as poverty, poor
+                    housing and public health. I also taught community work and health promotion at Bournemouth
+                    University. I retired from full-time work in 2015 at the age of sixty. In my retirement I have
+                    carried on what has been a lifelong commitment to voluntary work. I worked one afternoon a week last
+                    year, alongside three other volunteers, aboard the Bibby Stockholm barge, helping refugees to learn
+                    conversational English (I live on the Isle of Portland where the barge was moored). Most recently I
+                    worked with the museum here on the island to develop an innovative history project in partnership
+                    with the local primary school. We were all very proud of this project and although it is not
+                    especially relevant to this website, if you are interested, you can see an entertaining video about
+                    it here:
+                    <a href="https://www.stgeorgesportland.co.uk/">https://www.stgeorgesportland.co.uk/</a>
+                </p>
+                <p>
+                    I keep fit by walking along the coastline and among the strangely beautiful heaps of rock and
+                    abandoned quarries that make up much of Portland. I also play football twice a week - albeit at a
+                    slower pace than I used to play forty or fifty years ago - with the Portland Walking Football group.
+                </p>
+                <p>
+                    I have all sorts of other interests - but the most important in this context is that I have for many
+                    years collected old diaries, manuscripts, letters, photo albums and other socio-historical ephemera.
+                    I buy them wherever I find them; in auctions, in bookshops, at car boot sales. I always planned that
+                    in my retirement I would bring some of the personal stories I had discovered into the public domain.
+                    My research skills and experience of writing campaign materials and policy documents in my
+                    pre-retirement life proved invaluable in organising for publication the two books I have created so
+                    far.
+                </p>
+                <p>
+                    You can read about both books in the by clicking on the link at the top of the page. The two books
+                    are unlike each other in many ways but they have this in common; both are engaging human stories -
+                    and both offer fascinating insights into particular aspects of history. They are stories of amiable,
+                    ordinary people who have found a way to speak to us directly and vividly from the past. They are the
+                    voices of ghosts. Amiable ghosts.
                 </p>
             </div>
         </div>
