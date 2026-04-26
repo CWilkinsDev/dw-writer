29a0ef7a9a7a4cd8f66214908b703d2aaf5c6674
Charlie Wilkins <charlie@charliewilkins.tech>
2026-04-26 13:49:56 +0200

    Add SSGA page

--- public_html/schoolboy-servant.php
+++ public_html/schoolboy-servant.php
@@ -21,26 +21,120 @@
             <div class="pagetitle">
                 <h2>Schoolboy, Servant, GWR Apprentice</h2>
             </div>
-            <div class="contentblock">
+            <div class="contentblock" id="floatingimagecontainer">
+                <p><b>
+                        The memoirs of Alfred Plumley, 1810 - 1892
+                    </b></p>
                 <p>
-                    Alfred Plumley, son of a coachman, was born in 1874 in Somerset's Mendip Hills. Written in his old
-                    age, this memoir of his youth was discovered in an auction sale. In it, Alfred vividly describes his
-                    country childhood and first job as a serving boy at the grand house on the hill above his village.
-                    At age 16, Alfred decides to improve his prospects by 'going on the railway' and is sent to a tiny
-                    village station on the Somerset coast. He quickly comes to love his new life and, undeterred by an
-                    unhappy temporary posting to the grim and chaotic engine yards of Bristol, ends up spending
-                    forty-five years as a GWR employee.
+                    On a cold, thoroughly miserable afternoon in January 2014, in scarf and woolly hat, my warm coat
+                    zipped up, I was rooting around in the lots for sale at an auction saleroom on the Dorset coast. The
+                    wind howling off the sea rattled the windows. It felt colder indoors than outdoors.
                 </p>
                 <p>
-                    Alfred writes charmingly, and always with the authentic voice of a West Country lad. His memoir has
-                    been edited by David Wilkins who adds just the right amount of detail to place the story in its
-                    proper historical context.
+                    After about half an hour, I was beginning to wish I'd not bothered coming, Then I picked up a
+                    creased cardboard folder, clearly some decades old. It contained maybe a hundred typewritten sheets
+                    held
+                    together in small batches by rusted staples. The paper was tatty and discoloured. The wonky lines
+                    suggested that the typewriter's best days were already behind it when the typescript was made. As my
+                    cold-numbed fingers turned the pages, the document was revealed to be a memoir of some kind, written
+                    by an elderly gent recalling his rural childhood and adolescence. At the sale the following day, a
+                    single bid of £20.00 was enough. I was the only buyer interested.
+                </p>
+                <p>
+                    That evening I sat down to read the memoir. The bare bones of it are easily summarised. It was
+                    written in the first person - but anonymously. It described the life of a boy growing up in a
+                    village in the Mendip Hills in Somerset in the last quarter of the 19th century. It began with the
+                    writer's first day of school at the age of five. Just after his twelfth birthday, his schooldays
+                    came to end and he went immediately into service as a page-boy at the " big house" nearby. At
+                    sixteen, with the help of the kindly lady of the house who wanted to encourage him into a trade, he
+                    took
+                    up a station staff apprenticeship with the Great Western Railway (G.W.R.). He was placed at a tiny
+                    village station, just inland from the Somerset coast. When the writer turned eighteen in 1892, his
+                    job
+                    with the G.W.R. was confirmed as permanent - and at that point the story ended. The memoirist
+                    assured
+                    the reader though, that he went on to become a successful railwayman. His entire working life was to
+                    be
+                    spent with the G.W.R.. </p>
+                <p>
+                    The typescript itself was undated but piecing together the clues, it was possible to work out that
+                    it had been written between 1954 and 1956, when the author was in his early eighties. But who was
+                    he? He named himself just twice in the 40,000 words of text, on both occasions reporting other
+                    people speaking to him. The first time he referred to himself only by the forename, "Alfred". The
+                    second time - in reference to his surname - only by the initial "P". The places where the he lived,
+                    went to school and worked were all given fictitious names - a little deceit that dawned on me only
+                    after I'd wasted half an afternoon poring over maps of Somerset. Worse still, because the place
+                    names were invented, there was of course, also the possibility that that "Alfred", and/or that
+                    surname initial "P", were also fabrications. It could easily have been that the memoirist would
+                    prove impossible ever to identify. If that had been the case, of course, then I could never have
+                    written my book. On page 84 of the typescript however, there was a crucial five digit number. This
+                    number - 15718 - proved to be the secret code that would unlock the mystery. It was the number that
+                    appeared on the writer's wage slip every week for forty-five years; the number that just possibly
+                    might give him a name. It was his G.W.R. staff number.
+                </p>
+                <div id="floatingimage" style="font-size: medium; max-width: 515px; padding-left: 15px;">
+                    <img src="images/schoolboy-servant/alfred-plumley-staff-number_500x287.jpg"
+                        alt="Alfred's entry in the G.W.R. staff ledger." /><br>
+                    <i>Alfred's entry in the G.W.R. staff ledger; the key to the whole story.</i>
+                </div>
+                <p>
+                    The staff records of the G.W.R going back to the middle of the nineteenth century are stored at the
+                    National Archives in Kew. The records were not written down sequentially by staff number, so the
+                    only way to find our man was to look up every individual "Alfred" who started work with the G.W.R.
+                    in the later 1880s or the early 1890s. After I'd ploughed through the first fifty or sixty ghosts of
+                    Victorian engine drivers and stokers, signalmen, porters, booking clerks, fitters and coachbuilders
+                    named Alfred, the number in the right hand column of the staff records suddenly matched the number
+                    in my notebook. There, blinking in the sunlight, after more than a century shut flat in the G.W.R.
+                    staff ledger, was the author of the memoir, Alfred James Plumley himself.
+                </p>
+                <p>
+                    A quick reference to the 1881 census records revealed the very same Alfred James Plumley, then a
+                    "scholar" aged six, the son of a coachman, living in Lower Langford, a village in the Mendip region
+                    of Somerset. It was amazing just how much detail flowed from this first discovery. Alfred's
+                    manuscript did not give any real names, even to members of his own family - but public records of
+                    the period allowed me to identify them. Likewise I was able to give names to some of the village
+                    characters whom Alfred mentions. I was even able to identify the two grand houses at which Alfred
+                    worked as a servant by cross-referencing Alfred's clear written descriptions with old photographs of
+                    the local area. Gradually, an entire cast of real-life characters emerged to populate Alfred's
+                    story.
+                </p>
+                <div id="floatingimage" style="font-size: medium; max-width: 515px; padding-left: 15px;">
+                    <img src="images/schoolboy-servant/wsm-station-staff_500x347.jpg"
+                        alt="Station staff at Weston-super-Mare Station in the early 1890s." /><br>
+                    <i>Station staff at Weston-super-Mare Station in the early 1890s. Alfred's first G.W.R. job (1891 -
+                        1893) was at a village station just two miles up the line.</i>
+                </div>
+                <p>
+                    For Alfred, the past is a good place. His family is a happy and loving one. His mother is kind, his
+                    father is hard-working and respected in the village. Somerset's summer skies are always blue.
+                    Somerset's winter landscape glitters white with frost. The Mendip Hills are endlessly beautiful and
+                    the tumbling streams are crystal clear. His pals at school are boisterous and fun-loving, like small
+                    boys should be. When he goes to work at the big house, the playful maids treat him like their
+                    favoured little brother and the butler turns out to have a Jeeves-esque range of endearing hidden
+                    talents. When his G.W.R. apprenticeship calls for him to move away from home, his landlady is
+                    motherly, the station at which he works buzzes with cheerful rural activity and his station master
+                    proves to be a wise and benevolent man.
+                </p>
+                <p>
+                    <i>Schoolboy, Servant, G.W.R. Apprentice</i> takes a gentle stroll through Alfred's life, from the
+                    day he
+                    started school in 1879, to the day in 1892 when he became, officially and permanently, in his own
+                    words, "a uniformed servant of the mighty G.W.R.". Alfred's prose style is simple - almost
+                    conversational - but always expressive and engaging. He describes village life vividly and the
+                    Mendip landscape with great affection. His memories are full of human interest and of course, well
+                    over century on, the historical detail is fascinating.
                 </p>
                 <p>
                     <a href="https://thehistorypress.co.uk/publication/schoolboy-servant-gwr-apprentice/">
                         <i>Schoolboy, Servant, GWR Apprentice</i> is published by History Press.
                     </a>
                 </p>
+                <div style="font-size: medium;">
+                    <img src="images/schoolboy-servant/mendip-lodge_cropped_1080x737.jpg"
+                        alt="Mendip Lodge (since demolished), one of two grand houses locally where Alfred was employed as a servant." /><br>
+                    <i>Mendip Lodge (since demolished), one of two grand houses locally where Alfred was employed as a
+                        servant.</i>
+                </div>
             </div>
         </div>
         <div class="sidespacer"></div>
